73652726d7cc550bdb29c1fb70b7b53a2ac120ad
Armand SADY <armand.sady.etu@univ-lille.fr>
2023-04-11 09:52:04 +0200

    CriterionName & Criterion

--- dev/src/CriterionName.java
+++ dev/src/CriterionName.java
@@ -1,3 +1,33 @@
 public enum CriterionName {
-    
+    GUEST_HAS_ALLERGY('B'),
+    HOST_HAS_ANIMAL('B'),
+    GUEST_FOOD('T'),
+    HOST_FOOD('T'),
+    HOBBIES('T'),
+    GENDER('T'),
+    PAIR_GENDER('T'),
+    HISTORY('T');
+
+    /**
+     * Attributes
+     */
+    private final char TYPE;
+
+
+    /**
+     * Constructeur de CriterionName
+     * @param type
+     */
+    CriterionName(char type) {
+        this.TYPE = type;
+    }
+
+
+    /**
+     * getter du type du Cr
+     * @return
+     */
+    public char getType() {
+        return this.TYPE;
+    }
 }
