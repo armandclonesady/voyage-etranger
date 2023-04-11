b77db753b8b207b77e8b1dce3a8934d6f60523f9
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2023-04-11 09:46:21 +0200

    Begin test class

---
+++ dev/test/CriterionNameTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CriterionNameTest {
+
+    @Test
+    void getTypeTest() {
+        assertEquals('B', CriterionName);
+
+}
