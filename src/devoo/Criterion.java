fa5c78dc2393939a13fb6c07016a027dce74767e
Antoine <antoine.gaienier.etu@univ-lille.fr>
2023-05-05 14:13:01 +0200

    CLass test

--- src/devoo/Criterion.java
+++ src/devoo/Criterion.java
@@ -8,22 +8,16 @@ import java.util.Arrays;
  * @autor : Raphael Kiecken, Armand Sady, Antoine Gaienier
  */
 public class Criterion {
-    /**
-     * Attributes
-     */
+    /* Attributes */
     private CriterionName label;
     private String value;
 
-    /**
-     * Valeurs possible pour les critéres booléens
-     */
+    /* Valeurs possible pour les critéres booléens */
     final static public String POS = "yes"; 
     final static public String NEG = "no";
     final static private ArrayList<String> B_VALUES = new ArrayList<String>(Arrays.asList(POS, NEG));
 
-    /**
-     * Valeurs possible pour les critéres lié au Genre
-     */
+    /* Valeurs possible pour les critéres lié au Genre */
     final static public String F = "female"; 
     final static public String M = "male";
     final static public String OTH = "other";
@@ -31,23 +25,20 @@ public class Criterion {
     final static private ArrayList<String> GENDER_VALUES = new ArrayList<String>(Arrays.asList(F, M, OTH));
     final static private ArrayList<String> PAIR_GENDER_VALUES = new ArrayList<String>(Arrays.asList(F, M, OTH, BLANK));
 
-    /**
-     * Valeurs possible pour le critéres lié à l'Historique
-     */
+    /** Valeurs possible pour le critéres lié à l'Historique*/
     final static public String PREF_SAME = "same";
     final static public String PREF_OTH = "other";
     final static public String PREF_BLANK = "";
     final static private ArrayList<String> HISTORY_VALUES = new ArrayList<String>(Arrays.asList(PREF_SAME, PREF_OTH, PREF_BLANK));
 
-    /**
-     * Valeurs possible pour les critéres lié au Regime alimentaire
-     */
+    /* Valeurs possible pour les critéres lié au Regime alimentaire */
+    
     final static public String FOOD_N = "nonuts";
     final static public String FOOD_V = "vegetarian";
     final static public String FOOD_BLANK = "";
     final static private ArrayList<String> FOOD_VALUES = new ArrayList<String>(Arrays.asList(FOOD_N, FOOD_V, FOOD_BLANK));
 
-    /**
+    /* constructeur dr
      * @param label Nom du Criterion.
      * @param value Valeur du Criterion.
      */
--- test/devoo/TeenagerTest.java
+++ test/devoo/TeenagerTest.java
@@ -3,6 +3,28 @@ package devoo;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class TeenagerTest {
+
+    Teenager t1= new Teenager("t1", LocalDate.of(2000, 1, 1), Country.FRANCE);
+    Teenager t2= new Teenager("t2", LocalDate.of(2002, 1, 1), Country.FRANCE);
+    Teenager t3= new Teenager("t3", LocalDate.of(2004, 1, 1), Country.ITALY);
+    Teenager t4= new Teenager("t4", LocalDate.of(2000, 1, 1), Country.ITALY);
+    Teenager t5= new Teenager("t5", LocalDate.of(2000, 1, 1), Country.SPAIN);
+    Teenager t6= new Teenager("t6", LocalDate.of(2003, 12, 20), Country.SPAIN);
+    Teenager t7= new Teenager("t7", LocalDate.of(2000, 10, 11), Country.GERMANY);
+    Teenager t8= new Teenager("t8", LocalDate.of(2000, 4, 9), Country.GERMANY);
+
+
+    t1.addCriterion(new Criterion("GUEST_HAS_ALLERGY", "yes"));
+    t2.addCriterion(new Criterion("HOST_HAS_ANIMAL", "no"));
+
+    @Test
+    public void compatibleWithGuesTest() {
+        assertTrue(t1.compatibleWithGuest(t2));
+    }
 }
