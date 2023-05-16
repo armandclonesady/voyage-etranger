10f7b2a96bb829502a6103b016e258dd2a696508
antoine.gaienier <antoine.gaienier.etu@univ-lille.fr>
2023-05-16 10:19:03 +0200

    partage

--- src/graphes/AffectationUtil.java
+++ src/graphes/AffectationUtil.java
@@ -70,4 +70,41 @@ public class AffectationUtil {
         }
         return dico;
     }
+
+    public static void main(String[] args) {
+        Teenager t1, t2, t3, t4, t5, t6;
+
+        t1= new Teenager("A", LocalDate.of(2000, 1, 1),Country.FRANCE);
+        t2= new Teenager("B", LocalDate.of(2000, 1, 1),Country.FRANCE);
+        t3= new Teenager("C", LocalDate.of(2000, 1, 1),Country.FRANCE);
+        t4= new Teenager("X", LocalDate.of(2000, 1, 1),Country.ITALY);
+        t5= new Teenager("Y", LocalDate.of(2000, 1, 1),Country.ITALY);
+        t6= new Teenager("Z", LocalDate.of(2000, 1, 1),Country.ITALY);
+
+        t1.updateCriterion(new Criterion("HOST_HAS_ANIMAL", "no"));
+        t2.updateCriterion(new Criterion("HOST_HAS_ANIMAL", "yes"));
+        t3.updateCriterion(new Criterion("HOST_HAS_ANIMAL", "no"));
+        t1.updateCriterion(new Criterion("GUEST_HAS_ALLERGY", "no"));
+        t2.updateCriterion(new Criterion("GUEST_HAS_ALLERGY", "yes"));
+        t3.updateCriterion(new Criterion("GUEST_HAS_ALLERGY", "no"));
+        t4.updateCriterion(new Criterion("HOST_HAS_ANIMAL", "no"));
+        t5.updateCriterion(new Criterion("HOST_HAS_ANIMAL", "yes"));
+        t6.updateCriterion(new Criterion("HOST_HAS_ANIMAL", "no"));
+        t4.updateCriterion(new Criterion("GUEST_HAS_ALLERGY", "no"));
+        t5.updateCriterion(new Criterion("GUEST_HAS_ALLERGY", "yes"));
+        t6.updateCriterion(new Criterion("GUEST_HAS_ALLERGY", "no"));
+
+        List<Teenager> host = new ArrayList<Teenager>();
+        host.add(t1);
+        host.add(t2);
+        host.add(t3);
+
+        List<Teenager> guest = new ArrayList<Teenager>();
+        guest.add(t4);
+        guest.add(t5);
+        guest.add(t6);
+
+        System.out.println(init(host, guest)+ "\n");
+        System.out.println(compatibilityMap(host,guest));
+    }
 }
