3a6ba8b6c4bfe3fdd03627842db9f8d29196a6c2
Sora0432 <raphael.kiecken.etu@univ-lille.fr>
2023-06-12 03:11:10 +0200

    Remove unused import

--- src/ihm/AppController.java
+++ src/ihm/AppController.java
@@ -6,25 +6,18 @@ import java.util.Map;
 import devoo.Country;
 import devoo.IdComparator;
 import devoo.Teenager;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ListChangeListener.Change;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.web.WebHistory.Entry;
 
 public class AppController implements EventHandler<ActionEvent> {
     @FXML
